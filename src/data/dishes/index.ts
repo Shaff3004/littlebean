d80558c36b09f077bb5b5120ae118f73c681b513
Serhii.Volodin <serhii.volodin@tui.co.uk>
2026-07-22 22:34:16 +0300

    NOTASK added menu configuration through json files

--- src/data/dishes/index.ts
+++ src/data/dishes/index.ts
@@ -1,10 +1,17 @@
-import { breakfast } from "./breakfast";
-import { lunch } from "./lunch";
-import { snack } from "./snack";
-import { dinner } from "./dinner";
-import { desserts } from "./desserts";
-import { drinks } from "./drinks";
+import breakfastData from "./breakfast.json";
+import lunchData from "./lunch.json";
+import snackData from "./snack.json";
+import dinnerData from "./dinner.json";
+import dessertsData from "./desserts.json";
+import drinksData from "./drinks.json";
 import type { CategoryId, Dish } from "@/types";
+
+const breakfast = breakfastData as Dish[];
+const lunch = lunchData as Dish[];
+const snack = snackData as Dish[];
+const dinner = dinnerData as Dish[];
+const desserts = dessertsData as Dish[];
+const drinks = drinksData as Dish[];
 
 export const allDishes: Dish[] = [
   ...breakfast,
