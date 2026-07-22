fa5c770d1be817706a3b68f88afd3e74801b35c8
Serhii.Volodin <serhii.volodin@tui.co.uk>
2026-07-22 23:49:29 +0300

    NOTASK added google forms link

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,15 +1,2 @@
-/**
- * Replace this with your actual Google Form URL.
- * The URL should include the prefill parameter for the dish name field.
- *
- * Example:
- * https://docs.google.com/forms/d/e/1FAIpQLSf.../viewform?usp=pp_url&entry.123456=
- *
- * To find your form field entry ID:
- * 1. Open your Google Form
- * 2. Click "Get pre-filled link"
- * 3. Fill in a value and generate the link
- * 4. Copy the entry.XXXXX parameter from the URL
- */
 export const GOOGLE_FORM_BASE_URL =
-  "https://docs.google.com/forms/d/e/YOUR_FORM_ID/viewform?usp=pp_url&entry.FIELD_ID=";
+  "https://docs.google.com/forms/d/e/1FAIpQLSdy35v-dO1QAZJm8HT5C3G4RI1q_ihQiPEnITDGw8Fi7Vgp2A/viewform?usp=pp_url&entry.2019687599=";
